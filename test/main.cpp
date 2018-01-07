f600693b771e4f2d6e105e712bafbcc13314b271
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-01-07 18:05:49 +0100

    main: first test

--- test/main.cpp
+++ test/main.cpp
@@ -9,5 +9,18 @@ using namespace std;
 int main()
 {
     cout << "Hello World!\n";
+
+    // Initialization + static_cast
+    {
+        Float f{4.2f};
+        cout << f << endl;
+        Float g{3.2f};
+        float vf = static_cast<float>(g);
+        cout << vf << endl;
+    }
+
+    
+
+
     return 0;
 }
