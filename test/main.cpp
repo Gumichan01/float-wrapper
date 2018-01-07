3f43cc3bc54a6ce7ca13a61a18a11f3f69c3d461
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-01-07 18:20:08 +0100

    main: Tests -> math operations

--- test/main.cpp
+++ test/main.cpp
@@ -19,8 +19,127 @@ int main()
         cout << vf << endl;
     }
 
-    
+    // Opposite value
+    {
+        Float f{4.2f};
+        Float fe{-4.2f};
 
+        if((-f).v != fe.v)
+        {
+            cerr << "failure @" << __FILE__ << ":" << __LINE__ << "\n";
+            return 1;
+        }
+    }
+
+    // prefix '+'
+    {
+        Float f{4.2f};
+        Float fe{4.2f};
+        Float fee{5.2f};
+
+        if((f++).v != fe.v)
+        {
+            cerr << "failure @" << __FILE__ << ":" << __LINE__ << "\n";
+            return 2;
+        }
+
+        if(f.v != fee.v)
+        {
+            cerr << "failure @" << __FILE__ << ":" << __LINE__ << "\n";
+            return 3;
+        }
+    }
+
+    // postfix '+'
+    {
+        Float f{4.2f};
+        Float fe{5.2f};
+
+        if((++f).v != fe.v)
+        {
+            cerr << "failure @" << __FILE__ << ":" << __LINE__ << "\n";
+            return 4;
+        }
+    }
+
+    // prefix '-'
+    {
+        Float f{4.2f};
+        Float fe{4.2f};
+        Float fee{3.2f};
+
+        if((f--).v != fe.v)
+        {
+            cerr << "failure @" << __FILE__ << ":" << __LINE__ << "\n";
+            return 5;
+        }
+
+        if(f.v != fee.v)
+        {
+            cerr << "failure @" << __FILE__ << ":" << __LINE__ << "\n";
+            return 6;
+        }
+    }
+
+    // postfix '-'
+    {
+        Float f{4.2f};
+        Float fe{3.2f};
+
+        if((--f).v != fe.v)
+        {
+            cerr << "failure @" << __FILE__ << ":" << __LINE__ << "\n";
+            return 7;
+        }
+    }
+
+    // '+'
+    {
+        Float f{4.2f};
+        Float fe{4.2f + 4.2f};
+
+        if((f + f).v != fe.v)
+        {
+            cerr << "failure @" << __FILE__ << ":" << __LINE__ << "\n";
+            return 8;
+        }
+    }
+
+    // '-'
+    {
+        Float f{4.2f};
+        Float fe{4.2f - 4.2f};
+
+        if((f - f).v != fe.v)
+        {
+            cerr << "failure @" << __FILE__ << ":" << __LINE__ << "\n";
+            return 9;
+        }
+    }
+
+    // '*'
+    {
+        Float f{4.2f};
+        Float fe{4.2f * 4.2f};
+
+        if((f * f).v != fe.v)
+        {
+            cerr << "failure @" << __FILE__ << ":" << __LINE__ << "\n";
+            return 10;
+        }
+    }
+
+    // '/'
+    {
+        Float f{10.0f};
+        Float fe{10.0f / 2.0f};
+
+        if((f / Float{2.0f}).v != fe.v)
+        {
+            cerr << "failure @" << __FILE__ << ":" << __LINE__ << "\n";
+            return 8;
+        }
+    }
 
     return 0;
 }
